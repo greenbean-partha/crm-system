65c71884e4f02780d7069ce560c61d0fdb2e7786
Partha Roy <r.partha.07@gmail.com>
2026-01-18 17:00:12 +0530

    feat(crm): connect django to postgres

--- services/crm-service/app/settings.py
+++ services/crm-service/app/settings.py
@@ -74,10 +74,17 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "crm",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "postgres-crm",
+        "PORT": "5432",
     }
+}
+
 }
 
 
