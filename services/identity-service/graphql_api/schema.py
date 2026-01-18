a3098da0d4091255e69605c2ca3e55702f4e7ae1
Partha Roy <r.partha.07@gmail.com>
2026-01-18 20:04:08 +0530

    feat(identity): add company model and graphql query

--- services/identity-service/graphql_api/schema.py
+++ services/identity-service/graphql_api/schema.py
@@ -1,9 +1,16 @@
 import strawberry
+from typing import List, Optional
+from .models import Company
+
+@strawberry.type
+class CompanyType:
+    id: int
+    name: str
+    parent_id: Optional[int]
 
 @strawberry.type
 class Query:
     @strawberry.field
-    def health(self) -> str:
-        return "identity ok"
-
+    def companies(self) -> List[CompanyType]:
+        return Company.objects.all()
 schema = strawberry.Schema(query=Query)
